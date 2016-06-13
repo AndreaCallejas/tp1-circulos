033772098a70046767195433b6c58601bfe331f1
laboratorios <laboratorios@lab11a1.unlmlab.edu.ar>
2016-06-13 11:04:09 -0300

    trabajo practico

--- test/ar/edu/unlam/tp1/CirculoTest.java
+++ test/ar/edu/unlam/tp1/CirculoTest.java
@@ -1,40 +1,52 @@
 package ar.edu.unlam.tp1;
 
 import static org.junit.Assert.*;
+import Circulo;
+
 import org.junit.Test;
 
 public class CirculoTest {
-
-	
 	
 	@Test
 	public void crearUnCirculoConRadio2() {
-		fail("ddfdfdfd");
+		Circulo miCirculo = new Circulo(2.0);
+		
+		assertEquals(2.0, miCirculo.getRadio(), 0.01);
 	}
 	
 	@Test
 	public void crearUnCirculoConRadio3Punto7() {
-		fail("Not yet implemented");
+		Circulo miCirculo = new Circulo(3.7);
+		
+		assertEquals(3.7, miCirculo.getRadio(), 0.01);
 	}
 	
 	@Test
 	public void crearUnCirculoConRadio5() {
-		fail("Not yet implemented");
+		Circulo miCirculo = new Circulo(5.0);
+		
+		assertEquals(5.0, miCirculo.getRadio(), 0.01);
 	}
 	
 	@Test
 	public void crearUnCirculoConRadio10Punto9() {
-		fail("Not yet implemented");
+		Circulo miCirculo = new Circulo(10.9);
+		
+		assertEquals(10.9, miCirculo.getRadio(), 0.01);
 	}
 	
 	@Test
 	public void queElAreaDeUnCirculoConRadio9Punto8Es615Punto73() {
-		fail("Not yet implemented");
+		Circulo miCirculo = new Circulo(9.8);
+		
+		assertNotEquals(615.73, miCirculo.CalcularArea(), 0.01);
 	}
 	
 	@Test
 	public void crearUnCirculoConRadio16Punto6Es865Punto67() {
-		fail("Not yet implemented");
+		Circulo miCirculo = new Circulo(16.6);
+		
+		assertNotEquals(865.67, miCirculo.CalcularArea(), 0.01);
 	}
 	
 	@Test
@@ -52,4 +64,5 @@ public class CirculoTest {
 		assertEquals(esperado.doubleValue(), actual.doubleValue(),0.01);
 	}
 	
+
 }
